8bdcce1cd31e81f2338698a7d351a52569d4b3ab
Evgeny <evgeniy.mudryy@inbox.ru>
2022-05-01 21:31:29 +0300

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import styles from "./App.module.scss";
-function App() {
-  return <div className={styles.App}></div>;
-}
+import RequireAuth from "./hoc/RequireAuth";
+import { Login } from "./pages/Login";
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <div>main</div>
+                </RequireAuth>
+              }
+            />
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+    </div>
+  );
+};
 
 export default App;

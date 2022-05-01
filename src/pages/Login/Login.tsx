07632e01da004540a70d5ab98cdd06008c7d1bd3
Evgeny <evgeniy.mudryy@inbox.ru>
2022-05-01 21:31:18 +0300

    added mui + login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { LoginForm } from "../../components/LoginForm";
+import { useAuthActions } from "../../store/auth/authActions";
+import styles from "./Login.module.scss";
 
 const Login = () => {
+  const { login } = useAuthActions();
   return (
-    <div>
-      <LoginForm />
+    <div className={styles.page}>
+      <LoginForm isSubmittingForm={false} onSubmit={login} />
     </div>
   );
 };

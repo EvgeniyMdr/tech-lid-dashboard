84cda6ad29f74928722311f1dc62c7f14eb843f1
Evgeny <evgeniy.mudryy@inbox.ru>
2022-05-18 21:28:35 +0300

    small fixes

--- src/pages/EmployerPage/EmployerPage.tsx
+++ src/pages/EmployerPage/EmployerPage.tsx
@@ -1,10 +1,11 @@
 import { $employer, getEmployerFx } from "@/store/employer";
 import { deleteEmployerFx } from "@/store/employer/employer";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import { useStore } from "effector-react";
 import React, { useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import styles from "./EmployerPage.module.scss";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const EmployerPage = () => {
   const navigate = useNavigate();
@@ -22,45 +23,52 @@ const EmployerPage = () => {
 
   return (
     <div className={styles.pageWrapper}>
-      {employer && (
-        <div className={styles.employerWrapper}>
-          <img
-            className={styles.avatar}
-            src={
-              employer.avatar
-                ? employer.avatar
-                : "https://www.seekpng.com/png/detail/428-4287240_no-avatar-user-circle-icon-png.png"
-            }
-            alt={employer.name}
-          />
-          <h2>{employer.name}</h2>
-          <p>{employer.positionAtWork}</p>
-          <p>{employer.currentProject}</p>
-          <ul>
-            {employer?.projects?.map((el) => (
-              <li key={el.id}>{el.text}</li>
-            ))}
-          </ul>
-          <ul>
-            {employer?.skills?.map((el) => (
-              <li key={el.id}>{el.text}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-      <div className={styles.controllers}>
-        <Link
-          className={styles.link}
-          replace={true}
-          to={`/edit-employer/${id}`}
-        >
-          <Button variant="contained" color="secondary">
-            Редактировать
+      <Link className={styles.link} replace={true} to={`/`}>
+        <IconButton>
+          <ArrowBackIcon />
+        </IconButton>
+      </Link>
+      <div className={styles.contentWrapper}>
+        {employer && (
+          <div className={styles.employerWrapper}>
+            <img
+              className={styles.avatar}
+              src={
+                employer.avatar
+                  ? employer.avatar
+                  : "https://www.seekpng.com/png/detail/428-4287240_no-avatar-user-circle-icon-png.png"
+              }
+              alt={employer.name}
+            />
+            <h2>{employer.name}</h2>
+            <p>{employer.positionAtWork}</p>
+            <p>{employer.currentProject}</p>
+            <ul>
+              {employer?.projects?.map((el) => (
+                <li key={el.id}>{el.text}</li>
+              ))}
+            </ul>
+            <ul>
+              {employer?.skills?.map((el) => (
+                <li key={el.id}>{el.text}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <div className={styles.controllers}>
+          <Link
+            className={styles.link}
+            replace={true}
+            to={`/edit-employer/${id}`}
+          >
+            <Button variant="contained" color="secondary">
+              Редактировать
+            </Button>
+          </Link>
+          <Button onClick={deleteHandler} variant="contained" color="error">
+            Удалить
           </Button>
-        </Link>
-        <Button onClick={deleteHandler} variant="contained" color="error">
-          Удалить
-        </Button>
+        </div>
       </div>
     </div>
   );
